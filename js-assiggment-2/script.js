3684856115cb119a5513ccb85b420d3d4ce0ca76
jawadahmed01 <jawadqubist00@gmail.com>
2024-02-24 14:11:16 +0500

    task

---
+++ js-assiggment-2/script.js
@@ -0,0 +1,23 @@
+// Create a function called displayWelcomeMessage that prompts the user to enter their name and then displays a personalized welcome message such as "Welcome, [name]!" in an alert.
+
+// const stringName = prompt("Enter your name")
+
+// function displayname(string) {
+//   alert("Welcome " + string)
+// }
+
+// displayname(stringName)
+
+// let quote = ["Life is what happens while you are busy making other plans.", "Not all those who wander are lost.", "Nothing is certain except for death and taxes.","“Don't worry about failure; you only have to be right once.","Everything you've ever wanted is sitting on the other side of fear."];
+// function quoteFunction(){
+//   const randomIndex = Math.floor(Math.random() * quote.length);
+//   const randomQut = quote[randomIndex];
+//   console.log(randomQut)
+// }
+// quoteFunction();
+
+// function RandomNumber(){
+//   let num = Math.ceil(Math.random() * 100);
+//   console.log(num);
+// }
+// RandomNumber();
---
+++ js-assigment1-1/script.js
@@ -0,0 +1,62 @@
+// const val1 = +prompt("Enter your First Number")
+// const val2 = +prompt("Enter your Second Number")
+
+// function sum(number1, number2) {
+//   console.log("Sum of two number", number1 + number2)
+// }
+
+// sum(val1, val2)
+
+// const numberArry = [2, 3, 44, 56, 34]
+
+// function sumArray(arr) {
+//   let sum = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i]
+//   }
+//   return sum
+// }
+
+// console.log(sumArray(numberArry))
+
+// const numberArry = [2, 3, 44, 56, 34]
+
+// function average(arr) {
+//   let total = arr.length
+//   let sum = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i]
+//   }
+
+//   return sum / total
+// }
+
+// console.log(average(numberArry))
+
+// Swap The Character
+
+// const stringName = "name"
+
+// function swap(string) {
+//   const firstChar = string[0]
+//   const lastChar = string[string.length - 1]
+
+//   const swapTotal = lastChar + string.slice(1, -1) + firstChar
+
+//   console.log(swapTotal)
+// }
+
+//Compare strings by count of characters. For example, car and bat are equal. car and care are not.
+
+// const str1 = prompt("Enter your first value")
+// const str2 = prompt("Enter your second value")
+
+// function campareString(string) {
+//   if (str1.length === str2.length) {
+//     console.log("Your Character are equal")
+//   } else {
+//     console.log("Your are not equal ")
+//   }
+// }
+
+// campareString(str1, str2)
